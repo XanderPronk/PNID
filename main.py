0fb1b9d3a8e1d056aec6a68dba389900a707ec3c
xander <xander.c.pronk@gmail.com>
2023-12-20 12:52:42 +0000

    commit

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+class Test_class:  
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def get_sum(self):
+        return self.a + self.b
+    def get_difference(self):
+        return self.a - self.b
+    def get_product(self):
+        return self.a * self.b
+    def get_quotient(self):
+        return self.a / self.b
+    def test_fun(self):
+        return True
+
+
 if __name__ == "__main__":
     print("test")
 
---
+++ main_test_.py
@@ -0,0 +1,13 @@
+import unittest
+from main import Test_class
+
+class TestCalculations(unittest.TestCase):
+
+    def test_sum(self):
+        test = Test_class(8, 2)
+        self.assertEqual(test.get_sum(), 10, 'The sum is wrong.')
+        self.assertEqual(test.get_difference(), 6, 'The difference is wrong.')
+        self.assertEqual(test.test_fun(), False, "Not equal.")
+
+if __name__ == '__main__':
+    unittest.main()
